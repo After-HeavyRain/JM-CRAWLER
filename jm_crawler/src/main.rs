a86d5e2a2cc929fae40ec0fbd4c8e445dbef13a7
After-HeavyRain <dayu-gh@qq.com>
2024-03-18 00:17:49 +0800

    modified:   jm_crawler/Cargo.toml 	modified:   jm_crawler/src/main.rs

--- jm_crawler/src/main.rs
+++ jm_crawler/src/main.rs
@@ -1,35 +1,38 @@
-use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
+use reqwest::Result;
+use scraper::{Html, Selector};
 
-fn main() {
-    println!("Guess the number!");
+#[tokio::main]
+async fn main() -> Result<()> {
+    let url: &str = "https://jm365.work/mJ8rWd";
+    let user_agent: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0";
 
-    let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
-    
-    loop{
-        println!("Please input your guess.");
-    
-        let mut guess: String = String::new();
+    let client: reqwest::Client = reqwest::Client::new();
 
-        io::stdin().read_line(&mut guess)
-            .expect("Failed to read line");
-        
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
+    let resp = client
+        .get(url)
+        .header("User-Agent", user_agent)
+        .send()
+        .await?
+        .text()
+        .await?;
 
-        println!("You guessed:{}", guess);
+    //print!("{:#?}", resp);
 
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-            Ordering::Greater => println!("Too big"),
+    let windown: Html = Html::parse_document(&resp);
 
-        }
+    let range_selector = Selector::parse("div.word").unwrap();
+
+    //print!("{:#?}", windown.select(&range_selector));
+
+    let p_selector = Selector::parse("p").unwrap();
+    let br_selector = Selector::parse("br").unwrap();
+
+    for element in windown.select(&range_selector) {
+        let urls_element = element.select(&p_selector).next().expect("Could no select p");
+        let urls_element2 = urls_element.select(&br_selector).next().expect("Could no select br");
+        //let urls = urls_element.text();
+        println!("{:#?}", urls_element2.text());
     }
+
+    Ok(())
 }
